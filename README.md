8825ed0a0cc30ae1f6251c0cb0778faa54d6da85
Edwin Wargo <edwin@11thhournow.com>
2013-11-02 09:21:39 -0400

    First commit and README update

---
+++ README.md
@@ -0,0 +1,4 @@
+# Blocipedia
+
+A wiki application to demonstrate foundational programming and web development while in the Bloc apprenticeship. 
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blocipedia::Application.config.secret_token = '31257ccc8180321b801428a5616028ed5eaca33c88688ce2d6af4f1471e5741177998ce146e9ad2f15d3c4ff33a90ff2a77463bffcf6d3990b424e1a6938db1b'
